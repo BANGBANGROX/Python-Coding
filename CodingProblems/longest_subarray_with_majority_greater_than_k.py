0830b5b4a791bad642b369b13a7b7dafb5f25f7e
BANGBANGROX <lakshya0809bang@gmail.com>
2025-05-10 15:34:39 +0530

    Adds gfg POTD 10/05/2025

---
+++ CodingProblems/longest_subarray_with_majority_greater_than_k.py
@@ -0,0 +1,46 @@
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+# } Driver Code Ends
+
+# User function Template for python3
+class Solution:
+    def longestSubarray(self, arr, k):
+        first_index = {}
+        n = len(arr)
+        running_counter = 0
+        answer = 0
+
+        for i in range(n):
+            if arr[i] > k:
+                running_counter += 1
+            else:
+                running_counter -= 1
+            if running_counter > 0:
+                answer = i + 1
+            if first_index.get(running_counter - 1) is not None:
+                answer = max(answer, i - first_index[running_counter - 1])
+            if first_index.get(running_counter) is None:
+                first_index[running_counter] = i
+
+        return answer
+
+
+# Code Here
+
+
+# {
+# Driver Code Starts.
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = [int(x) for x in input().strip().split()]
+        k = int(input())
+
+        ob = Solution()
+        print(ob.longestSubarray(arr, k))
+        print("~")
+        t -= 1
+# } Driver Code Ends
